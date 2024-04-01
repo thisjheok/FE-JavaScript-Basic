f3bae006fd95e1d3ab0c1e0954911ea9bf42cfcd
thisjheok <jhjang3344@gmail.com>
2024-04-01 20:22:47 +0900

    3번

--- prob3.js
+++ prob3.js
@@ -1,1 +1,17 @@
 /* 3번 문제 답안을 작성해주세요. */
+function solution(arr) {
+    if(arr.length == 1){
+        arr=[-1];
+        return arr;
+    }
+    let min =Infinity;
+    let minIndex;
+    for(let i=0;i<arr.length;i++){
+        if(arr[i]<min){
+            min = arr[i];
+            minIndex = i;
+        }
+    }
+    arr.splice(minIndex,1);
+    return arr;
+}
