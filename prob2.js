af09f11ab7ec4c529e0dc1a3f735da951a1b562e
thisjheok <jhjang3344@gmail.com>
2024-04-01 19:46:26 +0900

    2번

--- prob2.js
+++ prob2.js
@@ -1,1 +1,10 @@
 /* 2번 문제 답안을 작성해주세요. */
+function solution(n) {
+    let fac = 1;
+    for(let i=1;i<=11;i++){
+        fac = fac*i;
+        if(fac>n){
+            return i-1;       
+        }
+    }
+}
